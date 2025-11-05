e8da24b821e5108d4cf0ed525063ae59f3e69dc2
Alaya <ralaya@uiowa.edu>
2025-11-05 15:20:49 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app, allow_unsafe_werkzeug=True)
